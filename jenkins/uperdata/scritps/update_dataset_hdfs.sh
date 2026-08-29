cb22f640f3ee1ec57f5630e7b24f0b403275945a
Santiago Andres David Gomez <sadavid@unicesar.edu.co>
2026-08-29 16:10:48 -0500

    fix routes

--- jenkins/uperdata/scritps/update_dataset_hdfs.sh
+++ jenkins/uperdata/scritps/update_dataset_hdfs.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 set -e
 
-DATA_DIR="/dataset"
 HDFS_PATH="/raw"
 
 echo "================================="
 echo " Subiendo datasets"
 echo "================================="
 
-# Crear la carpeta en HDFS si no existe
 docker exec namenode hdfs dfs -mkdir -p "$HDFS_PATH"
 
-for file in "$DATA_DIR"/*.csv
+for file in /dataset/*.csv
 do
     [ -e "$file" ] || continue
 
     filename=$(basename "$file")
     echo "Procesando: $filename"
 
-    docker cp "$file" namenode:/tmp/"$filename"
-    docker exec namenode hdfs dfs -put -f /tmp/"$filename" "$HDFS_PATH"
+    docker exec namenode hdfs dfs -put -f "$file" "$HDFS_PATH"
 
     if ! docker exec namenode hdfs dfs -test -f "$HDFS_PATH/$filename"; then
         echo "ERROR: $filename no se cargó correctamente en HDFS"
